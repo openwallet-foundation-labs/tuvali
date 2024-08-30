4c93d0ed25adcb06e8590678dea6ce4f13f900c0
Swati Goel <meet2swati@gmail.com>
2024-08-30 13:52:48 +0530

    convert this repository into pure android native library in kotlin
    Signed-off-by: Swati Goel <meet2swati@gmail.com>

--- android/src/main/java/io/mosip/tuvali/verifier/exception/VerifierException.kt
+++
@@ -1,6 +0,0 @@
-package io.mosip.tuvali.verifier.exception
-
-import io.mosip.tuvali.exception.BLEException
-import io.mosip.tuvali.exception.ErrorCode
-
-open class VerifierException(message: String,  cause: Exception? = null): BLEException(message, cause, errorCode = ErrorCode.VerifierUnknownException)
