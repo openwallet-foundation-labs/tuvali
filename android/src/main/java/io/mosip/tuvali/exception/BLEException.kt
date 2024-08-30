fd5cee61803eaa9e239587bc9a5e87b7ee4163d6
Swati Goel <meet2swati@gmail.com>
2024-08-30 13:52:48 +0530

    convert this repository into pure android native library in kotlin
    Signed-off-by: Swati Goel <meet2swati@gmail.com>

--- android/src/main/java/io/mosip/tuvali/exception/BLEException.kt
+++
@@ -1,4 +0,0 @@
-package io.mosip.tuvali.exception
-
-open class BLEException(message: String, cause: Exception?, val errorCode: ErrorCode): RuntimeException(message, cause) {
-}
--- android/src/main/java/io/mosip/tuvali/verifier/exception/TooManyFailureChunksException.kt
+++
@@ -1,6 +0,0 @@
-package io.mosip.tuvali.verifier.exception
-
-import io.mosip.tuvali.exception.BLEException
-import io.mosip.tuvali.exception.ErrorCode
-
-class TooManyFailureChunksException(s: String) : BLEException(s, null, ErrorCode.TooManyFailureChunksException)
